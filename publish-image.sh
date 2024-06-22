e6f42b79e9a4b65084a5ffa3209f41b17e6b4c75
aecomet <16721102+aecomet@users.noreply.github.com>
2024-06-22 15:14:41 +0900

    chore: add secret token to actions

--- publish-image.sh
+++ publish-image.sh
@@ -13,6 +13,9 @@ fi
 name=$1
 tag=$2
 
+# login to ghcr.io
+echo $CR_PAT | docker login ghcr.io -u aecomet --password-stdin
+
 # build & push image
 docker buildx build --push --platform linux/amd64 --no-cache -f "./$name/Dockerfile" -t "ghcr.io/aecomet/$name-base:$tag-amd64" ./$name
 docker buildx build --push --platform linux/arm64 --no-cache -f "./$name/Dockerfile" -t "ghcr.io/aecomet/$name-base:$tag-arm64" ./$name
@@ -21,4 +24,4 @@ docker buildx build --push --platform linux/arm64 --no-cache -f "./$name/Dockerf
 docker manifest create "ghcr.io/aecomet/$name-base:$tag" "ghcr.io/aecomet/$name-base:$tag-amd64" "ghcr.io/aecomet/$name-base:$tag-arm64"
 
 # push manifest
-docker manifest push "ghcr.io/aecomet/$name-base:$tag"
+docker manifest push -p "ghcr.io/aecomet/$name-base:$tag"
