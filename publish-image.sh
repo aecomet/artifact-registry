ec57c61b44cb57b3225ac0f19beab4dc5348d769
aecomet <16721102+aecomet@users.noreply.github.com>
2024-06-22 15:02:58 +0900

    fix: push to registry with buildx

--- publish-image.sh
+++ publish-image.sh
@@ -13,13 +13,9 @@ fi
 name=$1
 tag=$2
 
-# build image
-docker buildx build --platform linux/amd64 --no-cache -f "./$name/Dockerfile" -t "ghcr.io/aecomet/$name-base:$tag-amd64" ./$name
-docker buildx build --platform linux/arm64 --no-cache -f "./$name/Dockerfile" -t "ghcr.io/aecomet/$name-base:$tag-arm64" ./$name
-
-# publish image
-docker push "ghcr.io/aecomet/$name-base:$tag-amd64"
-docker push "ghcr.io/aecomet/$name-base:$tag-arm64"
+# build & push image
+docker buildx build --push --platform linux/amd64 --no-cache -f "./$name/Dockerfile" -t "ghcr.io/aecomet/$name-base:$tag-amd64" ./$name
+docker buildx build --push --platform linux/arm64 --no-cache -f "./$name/Dockerfile" -t "ghcr.io/aecomet/$name-base:$tag-arm64" ./$name
 
 # create manifest
 docker manifest create "ghcr.io/aecomet/$name-base:$tag" "ghcr.io/aecomet/$name-base:$tag-amd64" "ghcr.io/aecomet/$name-base:$tag-arm64"
